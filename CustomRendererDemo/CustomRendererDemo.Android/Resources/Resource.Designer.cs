545a98cfac3a9a47e1a4929792eab194cfa029e7
Houssem Dellai <houssem.dellai@live.com>
2017-09-24 22:10:44 +0200

    Added rounded_shape.xml

--- CustomRendererDemo/CustomRendererDemo.Android/Resources/Resource.Designer.cs
+++ CustomRendererDemo/CustomRendererDemo.Android/Resources/Resource.Designer.cs
@@ -2907,22 +2907,25 @@ namespace CustomRendererDemo.Droid
 			public const int notification_template_part_time = 2130903090;
 			
 			// aapt resource value: 0x7f030033
-			public const int select_dialog_item_material = 2130903091;
+			public const int rounded_shape = 2130903091;
 			
 			// aapt resource value: 0x7f030034
-			public const int select_dialog_multichoice_material = 2130903092;
+			public const int select_dialog_item_material = 2130903092;
 			
 			// aapt resource value: 0x7f030035
-			public const int select_dialog_singlechoice_material = 2130903093;
+			public const int select_dialog_multichoice_material = 2130903093;
 			
 			// aapt resource value: 0x7f030036
-			public const int support_simple_spinner_dropdown_item = 2130903094;
+			public const int select_dialog_singlechoice_material = 2130903094;
 			
 			// aapt resource value: 0x7f030037
-			public const int Tabbar = 2130903095;
+			public const int support_simple_spinner_dropdown_item = 2130903095;
 			
 			// aapt resource value: 0x7f030038
-			public const int Toolbar = 2130903096;
+			public const int Tabbar = 2130903096;
+			
+			// aapt resource value: 0x7f030039
+			public const int Toolbar = 2130903097;
 			
 			static Layout()
 			{
--- CustomRendererDemo/CustomRendererDemo.Android/RoundedEntryRendererAndroid.cs
+++ CustomRendererDemo/CustomRendererDemo.Android/RoundedEntryRendererAndroid.cs
@@ -9,20 +9,21 @@ namespace CustomRendererDemo.Droid
 {
     public class RoundedEntryRendererAndroid : EntryRenderer
     {
-
         protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
         {
             base.OnElementChanged(e);
 
             if (e.OldElement == null)
             {
-                var gradientDrawable = new GradientDrawable();
-                gradientDrawable.SetCornerRadius(60f);
-                gradientDrawable.SetStroke(5, Android.Graphics.Color.DeepPink);
-                gradientDrawable.SetColor(Android.Graphics.Color.LightGray);
-                Control.SetBackground(gradientDrawable);
-                
-                Control.SetPadding(50, Control.PaddingTop, Control.PaddingRight, Control.PaddingBottom);
+                Control.SetBackgroundResource(Resource.Layout.rounded_shape);
+                //    var gradientDrawable = new GradientDrawable();
+                //    gradientDrawable.SetCornerRadius(60f);
+                //    gradientDrawable.SetStroke(5, Android.Graphics.Color.DeepPink);
+                //    gradientDrawable.SetColor(Android.Graphics.Color.LightGray);
+                //    Control.SetBackground(gradientDrawable);
+
+                //    Control.SetPadding(50, Control.PaddingTop, Control.PaddingRight,
+                //Control.PaddingBottom);
             }
         }
     }
