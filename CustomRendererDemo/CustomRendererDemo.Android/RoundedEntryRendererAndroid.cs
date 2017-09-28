f36ca7f7e510a0fdcce0a35a2e376e653ad294fb
Houssem Dellai <houssem.dellai@live.com>
2017-09-28 20:01:03 +0200

    Added screenshot

--- CustomRendererDemo/CustomRendererDemo.Android/RoundedEntryRendererAndroid.cs
+++ CustomRendererDemo/CustomRendererDemo.Android/RoundedEntryRendererAndroid.cs
@@ -15,15 +15,15 @@ namespace CustomRendererDemo.Droid
 
             if (e.OldElement == null)
             {
-                Control.SetBackgroundResource(Resource.Layout.rounded_shape);
-                //    var gradientDrawable = new GradientDrawable();
-                //    gradientDrawable.SetCornerRadius(60f);
-                //    gradientDrawable.SetStroke(5, Android.Graphics.Color.DeepPink);
-                //    gradientDrawable.SetColor(Android.Graphics.Color.LightGray);
-                //    Control.SetBackground(gradientDrawable);
+                //Control.SetBackgroundResource(Resource.Layout.rounded_shape);
+                var gradientDrawable = new GradientDrawable();
+                gradientDrawable.SetCornerRadius(60f);
+                gradientDrawable.SetStroke(5, Android.Graphics.Color.DeepPink);
+                gradientDrawable.SetColor(Android.Graphics.Color.LightGray);
+                Control.SetBackground(gradientDrawable);
 
-                //    Control.SetPadding(50, Control.PaddingTop, Control.PaddingRight,
-                //Control.PaddingBottom);
+                Control.SetPadding(50, Control.PaddingTop, Control.PaddingRight,  
+                    Control.PaddingBottom);
             }
         }
     }
